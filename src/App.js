eccd5066467537305b5ef12b64dbd3f15f9a46d9
Matheus Leandro <mattleandroo@outlook.com>
2019-11-11 11:43:59 -0200

    configuring global styles and routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+
+import GlobalStyle from './styles/global';
+import Routes from './routes';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World</h1>
-    </div>
+    <BrowserRouter>
+      {/* <Header /> */}
+      <Routes />
+      <GlobalStyle />
+    </BrowserRouter>
   );
 }
 
