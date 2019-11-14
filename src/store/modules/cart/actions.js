fefb4a730beb93339654bae2354d7fe2734831d6
Matheus Leandro <mattleandroo@outlook.com>
2019-11-13 21:55:17 -0300

    configuring redux saga

--- src/store/modules/cart/actions.js
+++ src/store/modules/cart/actions.js
@@ -1,6 +1,13 @@
-export function addToCart(product) {
+export function addToCartRequest(id) {
   return {
-    type: '@card/ADD',
+    type: '@cart/ADD_REQUEST',
+    id,
+  };
+}
+
+export function addToCartSuccess(product) {
+  return {
+    type: '@cart/ADD_SUCCESS',
     product,
   };
 }
--- src/store/modules/cart/reducer.js
+++ src/store/modules/cart/reducer.js
@@ -2,7 +2,7 @@ import produce from 'immer';
 
 export default function cart(state = [], action) {
   switch (action.type) {
-    case '@card/ADD':
+    case '@cart/ADD_SUCCESS':
       return produce(state, draft => {
         const productIndex = draft.findIndex(p => p.id === action.product.id);
 
