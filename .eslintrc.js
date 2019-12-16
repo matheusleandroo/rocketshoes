9069777a1d74a3e1e1e611568a00e9ce6fa95d0a
Matheus Leandro <mattleandroo@outlook.com>
2019-12-16 20:33:50 -0300

    add hooks

--- .eslintrc.js
+++ .eslintrc.js
@@ -22,7 +22,8 @@ module.exports = {
   },
   plugins: [
     'react',
-    'prettier'
+    'prettier',
+    'react-hooks',
   ],
   rules: {
     'prettier/prettier': 'error',
@@ -33,6 +34,8 @@ module.exports = {
     'import/prefer-default-export': 'off',
     'react/state-in-constructor': 'off',
     'no-console': ["error", { allow: ["tron"] }],
-    'no-param-reassign': 'off'
+    'no-param-reassign': 'off',
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
   },
 };
